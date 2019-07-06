1a7bdb2b7aa0047db9de670eeb0167fce35cff21
Bilalkhan5869 <52583766+Bilalkhan5869@users.noreply.github.com>
2019-07-06 13:21:00 -0700

    Add files via upload
    Homework1

---
+++ Homework1.py
@@ -0,0 +1,34 @@
+# Title of the song
+Title         = "Dusk till dawn"
+# Album of the song
+Album         = "Icarus Falls"
+# Artist of the song
+Artist        = "Zayn Malik & Sia Furier"
+# Music Composer of the song
+Label         = "RCA"
+# Genre of the song
+Genre         = "Pop"
+# Producer
+Producer = "Greg Kurstin"
+#Duration of the song
+Duration = "4:27"
+# Release year
+Year          = 2017
+
+
+print("Title: ", Title)
+print("--------------------------")
+print("Album: ", Album)
+print("--------------------------")
+print("Artist: ", Artist)
+print("--------------------------")
+print("Label: ", Label)
+print("--------------------------")
+print("Genre: ", Genre)
+print("--------------------------")
+print("Producer: ", Producer)
+print("--------------------------")
+print("Duration: ", Duration)
+print("--------------------------")
+print("Year: ", Year)
+print("--------------------------")
